9498d17998e347776532967d4762d8c721cc96eb
deva40211 <devashish15262@gmail.com>
2023-12-23 10:57:41 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('connected to mongoDB');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+//.env is ignored by github, so it's safer
 
 const app = express();
 
