04ff01d3d5462b93c494245a84d79a896269213d
deva40211 <devashish15262@gmail.com>
2023-12-29 14:11:23 +0530

    final commit

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -53,7 +53,7 @@ export default function Home() {
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>Find your next <span className='text-slate-500'>perfect</span> <br />place with ease</h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Sahan Estate is the best place to find your next perfect place to live.
+          Deva Estate is the best place to find your next perfect place to live.
           <br />
           We have a wide range of prperties for you to choose from.
         </div>
